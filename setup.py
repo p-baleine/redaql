4dccb906e1aafae4dceba662b775e11ec3e7d35f
denzow <denzow@gmail.com>
2021-03-25 00:16:13 +0900

    [add] setup.cfg.install_requirements

--- setup.py
+++ setup.py
@@ -3,13 +3,8 @@ from setuptools import setup
 
 here = os.path.abspath(os.path.dirname(__file__))
 
-about = {}
-with open(os.path.join(here, 'redaql', '__version__.py'), 'r') as f:
-    exec(f.read(), about)
-
 packages = ['redaql']
 
 setup(
-    version=about['__VERSION__'],
     packages=packages,
 )
